e7ed21b5358bdc01d816a6355ee30408001b66b5
Elsa Suprano <elsamyjin@gmail.com>
2022-09-09 17:49:41 -0700

    homework20 update

--- homework20reactportfolio/src/App.js
+++ homework20reactportfolio/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar'
+import CssBaseline from '@mui/matrial/CssBaseline';
+import About from './components/About'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <CssBaseline />
+    <Navbar /> 
+    <About />
+
+    </>
   );
 }
 
 export default App;
+
